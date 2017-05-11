380555dc29c2757c2049260ad5f8bf4612eba8c5
Jormangandur <=>
2017-05-11 20:03:27 +0100

    Remove test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import tournament
-
-l = tournament.swissPairings()
-result = []
-
-for p1, p2 in zip(l[0::2], l[1::2]):
-    result.append((p1[0], p1[1], p2[0], p2[1]))
-
-print zip(l[0::2], l[1::2])
