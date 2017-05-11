3353ea004bf22923771cad2770b2afcf9294f959
Jormangandur <steadmanben1@gmail.com>
2017-05-11 19:01:48 +0000

    Initial commit of basic funcitonality

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tournament Results
+Python module using PostgreSQL database to keep track of players and matches in a game tournament.
+
+The game tournament uses the Swiss system for pairing up players in each round:
+* No player elimination
+* Players are paired with equal (or close to) wins each round
---
+++ tournament.sql
@@ -0,0 +1,44 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+-- player table
+-- match scores table
+
+CREATE TABLE players (name text,
+                      id serial primary key );
+
+CREATE TABLE matches (winner integer references players(id),
+                      loser integer references players(id),
+                      id serial PRIMARY KEY);
+
+CREATE VIEW standings AS
+                      SELECT players.id,players.name,
+                      (SELECT count(matches.winner)
+                      FROM matches
+                      WHERE matches.winner = players.id)
+                      AS wins,
+                      (SELECT count(matches.id)
+                      FROM matches
+                      WHERE players.id = matches.winner
+                      OR players.id = matches.loser)
+                      AS matches
+                      FROM players
+                      ORDER BY wins DESC;
+
+CREATE VIEW ranks AS
+                     SELECT
+                     id,
+                     name,
+                     ROW_NUMBER() OVER (ORDER BY wins DESC) as position
+                     FROM standings
+                     ORDER BY position;
+
+CREATE VIEW groups AS
+                   SELECT *,
+                   (position+1)/2 as group -- integer division -> (2+1)/2 = 1
+                   FROM ranks;
